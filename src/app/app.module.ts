8105e41c1f0d7be7d95be5d8090614b3a21c9523
onesBenRhaime <onesrhaime28@gmail.com>
2023-09-26 12:16:46 +0100

    Workshop 3 : Manipulation de composant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SlideComponent } from './slide/slide.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { FooterComponent } from './footer/footer.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SlideComponent,
     UserListComponent,
-    FooterComponent
+    FooterComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
