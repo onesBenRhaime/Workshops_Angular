15eeef82db6eadc72901194d17b1996d9cd99945
onesBenRhaime <onesrhaime28@gmail.com>
2023-09-26 10:43:26 +0100

    Workshop 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlideComponent } from './slide/slide.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlideComponent,
+    UserListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
